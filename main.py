373a393d04d10b9bf55f9b14b12dcf159fc7a78b
yassi <yassin.bouayad@ensta-bretagne.org>
2023-05-10 16:22:25 +0200

    nouvelle version

--- main.py
+++ main.py
@@ -4,11 +4,20 @@ import numpy as np
 ligue1=script_championnat.Championnat()
 ligue1.planifier_matches()
 ligue1.jouer_matches()
-A=np.zeros((len(ligue1.clubs),3))
-# for i in range(len(ligue1.clubs)):
-#     A[0,i]=i+1
-#     A[1,i]=(ligue1.clubs[i]).nom
-# #     A[2,i]=(ligue1.clubs[i]).points
-# print(A)
+cl=[]
+for i in range(len(ligue1.clubs)):
+     n=[]
+     n.append(ligue1.clubs[i].points)
+     n.append(ligue1.clubs[i].nom)
+
+     cl.append(n)
+cl.sort(reverse=True)
+for i in range(len(cl)):
+     cl[i].append(i+1)
+     a=cl[i][0]
+     b=cl[i][2]
+     cl[i][2]=a
+     cl[i][0]=b
+print(cl)
 
 
