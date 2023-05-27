36b12055a871c5b6be20cbb96382883113f39996
yassi <yassin.bouayad@ensta-bretagne.org>
2023-05-27 12:33:22 +0200

    nouvelle version

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QTextEdit, QMessageBox, QFileDialog, QTableWidget, QTableWidgetItem
 import script_championnat
-
+from matplotlib import pyplot as plt
 class ChampionnatInterface(QWidget):
     def __init__(self):
         super().__init__()
@@ -35,6 +35,7 @@ class ChampionnatInterface(QWidget):
 
         self.setLayout(self.layout)
 
+
     def calculer_championnat(self):
         self.ligue1 = script_championnat.Championnat()
         self.ligue1.planifier_matches()
@@ -45,20 +46,23 @@ class ChampionnatInterface(QWidget):
         resultat = "Classement du championnat :\n"
         clubs = self.ligue1.get_clubs()
 
-        # Trie des clubs par points
-        clubs_tries = sorted(clubs, key=lambda x: x.get_points(), reverse=True)
+
+        # On trie les clubs par points selon les points
+        clubs.sort(key=lambda x: x.get_points(), reverse=True)
+        # ici on utilise key=lambda pour créer une fonction 'inine' sinon on devrait faire unn boucle for pour recuperer les points puis trier les clubs
 
         # Parcours des clubs triés
-        for i, club in enumerate(clubs_tries):
-            resultat += f"{i+1}. Club : {club.get_nom()}\n"
-            resultat += f"Points : {club.get_points()}\n"
-            resultat += f"Buts marqués : {club.get_buts_marqués()}\n"
+        for i, club in enumerate(clubs):
+            resultat += "{}. Club : {}\n".format(i+1, club.get_nom())
+            resultat += "Points : {}\n".format(club.get_points())
+            resultat += "Buts marqués : {}\n".format(club.get_buts_marqués())
 
             joueurs = club.get_joueur()
             meilleur_joueur = max(joueurs, key=lambda x: x.get_buts_marqués())
-            resultat += f"Meilleur joueur : {meilleur_joueur.get_nom()} - Buts marqués : {meilleur_joueur.get_buts_marqués()}\n\n"
+            resultat += "Meilleur joueur : {} - Buts marqués : {}\n\n".format(meilleur_joueur.get_nom(),meilleur_joueur.get_buts_marqués())
 
         self.text_resultat.setPlainText(resultat)
+        self.creer_boutons_clubs()
 
     def sauvegarder_resultats(self):
         fichier = self.text_fichier.text()
@@ -76,6 +80,25 @@ class ChampionnatInterface(QWidget):
             QMessageBox.information(self, "Sauvegarde réussie", "Les résultats ont été sauvegardés avec succès.")
         except Exception as e:
             QMessageBox.warning(self, "Erreur", f"Erreur lors de la sauvegarde : {str(e)}")
+
+    def afficher_graphique_club(self, club):
+        joueurs = club.get_joueur()
+        noms_joueurs = [joueur.get_nom() for joueur in joueurs]
+        buts_marques = [joueur.get_buts_marqués() for joueur in joueurs]
+
+        plt.bar(noms_joueurs, buts_marques)
+        plt.xlabel("Joueurs")
+        plt.ylabel("Buts marqués")
+        plt.title("Statistiques des buts marqués par joueur")
+
+        plt.show()
+    def creer_boutons_clubs(self):
+        clubs = self.ligue1.get_clubs()
+
+        for club in clubs:
+            bouton_club = QPushButton(club.get_nom())
+            bouton_club.clicked.connect(lambda _, c=club: self.afficher_graphique_club(c))
+            self.layout.addWidget(bouton_club)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
