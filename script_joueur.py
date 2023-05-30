4033a3affbce7e3e93e17276d1d857e6dc1e821b
yassi <yassin.bouayad@ensta-bretagne.org>
2023-05-30 18:38:01 +0200

    nouvelle version

--- script_joueur.py
+++ script_joueur.py
@@ -1,8 +1,8 @@
 import numpy as np
 import random as r
 #heritage postes joueur attaquant defenseur
+#Classe mère joeurs
 class Joueur:
-
     def __init__(self,nom):
         self.nom=nom
         self.butsmarqués=0
@@ -11,40 +11,44 @@ class Joueur:
 
     def evol_buts(self,nb_buts):
         self.butsmarqués+=nb_buts
+# classe définie finalement dans la classe fille
     def evol_notejoueur(self,resultat):
         pass
-
-            #if resultat=='D':
-             #   self.note+=r.randint(-1,0)
-            #elif resultat=='N':
-             #   self.note+=r.randint(-1,1)
-            #else:
-             #   self.note+=r.randint(0,1)
-
+#Accesseurs
     def get_nom(self):
         return self.nom
+
     def get_buts_marqués(self):
         return self.butsmarqués
+
     def get_note(self):
         return self.note
 
+
+# classes filles on ne définie ici que la méthode de evolnotejoueurs
 class Attaquant(Joueur) :
     def __init__(self,nom):
         super().__init__(self)
+        #deux notes une d'attaque et une de defense
         self.note_att=r.randint(7,9)
         self.note_def=r.randint(3,5)
         self.note=(self.note_def+self.note_att)/2
+    #on évolue la note des joeurs en fonction de si ce sont des attaquants on augmente leur note si ils ont marqué plein de buts
     def evol_notejoueur(self,resultat):
         if resultat[0]>=1 and self.note_att<10:
             self.note_att+=0.5*r.randint(0,1)
         elif resultat[0]<=1 and self.note_att>0:
             self.note_att+=0.5*r.randint(-1,0)
+
+
 class Defenseur(Joueur):
     def __init__(self,nom):
         super().__init__(nom)
         self.note_att=r.randint(3,5)
         self.note_def=r.randint(7,9)
         self.note=(self.note_def+self.note_att)/2
+
+    # on évolue la note des joeurs en fonction de si ce sont des defenseurs on augmente leur notes si ils ont par pris beaucoup de buts
     def evol_notejoueur(self,resultat):
         if resultat[1] >= 2 and self.note_def>0:
             self.note_def += 0.5 * r.randint(-1, 0)
